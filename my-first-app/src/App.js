332702ef08caca7c8982ea5ecf64151241cad257
ankitasri20 <ankitasrivastava2504@gmail.com>
2025-06-03 19:19:18 +0530

    initial

---
+++ my-first-app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Header from "./Layout/Header";
+import Footer from "./Layout/Footer";
+
+
+const App = () => {
+  return (
+    <div className="anita">
+      
+      <h1>Ankita</h1>
+    <Header/>
+    <Footer/>
+      
+    </div>
+  );  
+}
+export default App;
